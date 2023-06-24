b1b149175e0fcf28da8244c9d18d54e722955c58
Alanood <s167959@ese.gov.ae>
2023-06-24 14:01:41 +0400

    Initial Commit

---
+++ alanood/alanoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  alanoodApp.swift
+//  alanood
+//
+//  Created by Alanood Almansoori on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct alanoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
